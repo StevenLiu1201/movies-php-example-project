19ba55e4f1ecd832885b5553c69895267eaf0fde
Zhongrui Liu <liu00642@algonquinlive.com>
2024-05-28 21:34:43 -0400

    part2 update

--- edit.php
+++ edit.php
@@ -1,5 +1,50 @@
 <?php
   require "data.php";
+
+
+  // you can is isset post to check, or use this to check if a form has been submit
+  if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $new = [
+      'movie_id' => $_POST['movie_id'], 
+      'movie_title' => $_POST['movie_title'],
+      'director' => $_POST['director'],
+      'year' => $_POST['year'],
+      'genre' => $_POST['genre']
+    ];
+
+    array_map(function($movie) use ($new){
+      
+      if($movie['movie_id'] == $new['movie_id']){
+        // return the new movie 
+        var_dump('return new');
+        
+        return $new;
+      }
+
+      // return the $movie
+      return $movie;
+    },$movies);
+
+    // update the session as well
+    $_SESSION['movies'] = $movies;
+
+    // get a problem, when i back to movie php, it did not get the value from session, still the elder data.
+    header("Location: movie.php?id=" . $_POST['movie_id']);
+  }
+
+
+  if (isset($_GET['id'])) {
+    $movie = current(array_filter($movies, function ($movie) {
+      return $movie['movie_id'] == $_GET['id'];
+    }));
+
+    if (!$movie) {
+      // go back to movie.php
+      header("Location: movie.php");
+    }
+  } else {
+    header("Location: movie.php");
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,12 +61,16 @@
     <h2 class="form-title">Edit Movie</h2>
     <form class="form" method="post">
       <input 
+          type="hidden"
+          name="movie_id"
+          value="<?php echo $movie['movie_id']; ?>"> <!--check with video -->
+      <input 
         type="text" 
         class="form-control" 
         name="movie_title" 
-        placeholder="Movie Title" 
+        placeholder="movie title" 
         required 
-        value="Labyrinth">
+        value="<?php echo $movie['movie_title']; ?>">
       <div class="error text-danger"></div>
       <input 
         type="text" 
@@ -29,7 +78,7 @@
         name="director" 
         placeholder="Director" 
         required
-        value="Jim Henson">
+        value="<?php echo $movie['director']; ?>">
       <div class="error text-danger"></div>
       <input 
         type="number" 
@@ -37,18 +86,27 @@
         name="year" 
         placeholder="Year" 
         required
-        value="1986">
+        value="<?php echo $movie['year']; ?>">
       <div class="error text-danger"></div>
       <select class="form-select" name="genre">
         <option value="">Select a Genre</option>
         <?php foreach ($genres as $genre) : ?>
-        <option value="<?php echo $genre; ?>">
+        <option value="<?php echo $genre; ?>"
+        <?php if ($genre == $movie['genre']) : ?> selected <?php endif; ?>>
           <?php echo $genre; ?>
         </option>
         <?php endforeach; ?>
       </select>
       <div class="error text-danger"></div>
       <button type="submit" class="button">Update Movie</button>
+    </form>
+
+    <form method="post" action="delete.php">
+          <input
+          type="hidden"
+          name="movie_id"
+          value="<?php echo $movie['movie_id']; ?>">
+          <button type="submit" class="button danger">delete</button>
     </form>
   </main>
 </body>
